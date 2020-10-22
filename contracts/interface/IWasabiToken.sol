3534ba09d6ab2e0e1d1d4e2b396c1e74c26a1de1
tercel <tercel.yi@gmail.com>
2020-10-22 13:34:48 +0800

    remove unused files

--- contracts/interface/IWasabiToken.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
-
-interface IWasabiToken {
-    function mint(address to) external returns (uint);
-    function increaseProductivity(address user, uint value) external returns (uint);
-    function decreaseProductivity(address user, uint value) external returns (uint);
-}
